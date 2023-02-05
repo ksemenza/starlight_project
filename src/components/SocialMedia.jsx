fceb778aa0a93f36f09a8a6eeeb015d3b53752de
unknown <ksemenza13@gmail.com>
2023-02-05 10:52:06 -0500

    600px media background image

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -2,14 +2,45 @@ import facebook from "../assets/icon/sticker/sticker_facebook.png";
 import instagram from "../assets/icon/sticker/sticker_instagram.png";
 import twitter from "../assets/icon/sticker/sticker_twitter.png";
 import youTube from "../assets/icon/sticker/sticker_youtube.png";
+import tikTok from "../assets/icon/sticker/sticker_tiktok.png";
+import spotify from "../assets/icon/sticker/sticker_spotify.png";
 
 function SocialMedia() {
   return (
     <div className="social-media-cta">
-      <img className="social-media-icon" src={facebook} alt="FACEBOOK" />
-      <img className="social-media-icon" src={instagram} alt="INSTAGRAM" />
+
+    {/* Facebook */}
+      <a href="https://www.facebook.com/felicity.blaney.9?mibextid=ZbWKwL" target="_blank">
+        {" "}
+        <img className="social-media-icon" src={facebook} alt="FACEBOOK" />
+      </a>
+
+          {/* Instagram */}
+      <a href="https://www.instagram.com/itsfelicityestelle/" target="_blank">
+        {" "}
+        <img className="social-media-icon" src={instagram} alt="INSTAGRAM" />
+      </a>
+
+          {/* Twitter */}
+      <a href="https://www.facebook.com/felicity.blaney.9?mibextid=ZbWKwL" target="_blank">
       <img className="social-media-icon" src={twitter} alt="TWITTER" />
+      </a>
+
+
+    {/* YouTube */}
+      <a href="https://youtube.com/@itsfelicityestelle" target="_blank">
       <img className="social-media-icon" src={youTube} alt="YOUTUBE" />
+      </a>
+      
+    {/* TikTok */}
+      <a href="https://www.tiktok.com/music/PC-7096449807597701122" target="_blank">
+      <img className="social-media-icon" src={tikTok} alt="TIKTOK" />
+      </a>
+
+          {/* Spotify */}
+      <a href="https://open.spotify.com/artist/5zTBOvOHk4AyiEqjzS9ZNx" target="_blank">
+      <img className="social-media-icon" src={spotify} alt="SPOTIFY" />
+      </a>
     </div>
   );
 }
