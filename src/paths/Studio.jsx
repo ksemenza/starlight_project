69ca0b1b7cb32c27784ab3576576e7d1879529c8
unknown <ksemenza13@gmail.com>
2023-02-05 15:55:15 -0500

    studio react-player video only

--- src/paths/Studio.jsx
+++ src/paths/Studio.jsx
@@ -1,10 +1,12 @@
 import '../assets/css/path.css';
+import ReactPlayer from 'react-player';
+
 
 function Studio () {
   return (
     <div className="path-cta studio-cta">
         <h2 className="path-h2">Studio</h2>
-
+        <ReactPlayer className="react-player" url='https://youtu.be/jP3Xs7WFPX0'/>
 
     </div>
   );
