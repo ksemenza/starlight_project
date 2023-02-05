efd870f58c42fc3b63e33d75d745a21644655683
unknown <ksemenza13@gmail.com>
2023-02-05 07:33:57 -0500

    path ui test

--- src/root/headerRoot.jsx
+++ src/root/headerRoot.jsx
@@ -1,17 +1,15 @@
-import '../assets/css/header.css';
-import NavBar from '../components/NavBar'
-import SocialMedia from '../components/SocialMedia'
+import "../assets/css/header.css";
+import NavBar from "../components/NavBar";
+import SocialMedia from "../components/SocialMedia";
 
 function headerRoot() {
   return (
     <div className="header-root-cta">
-    <h1 className='header-h1'>Eloquent Star</h1>
-    <div className='social-nav-cta'>
-
-    <NavBar/>
-    <SocialMedia/>
-
-    </div>
+      <h1 className="header-h1">Eloquent Star</h1>
+      <div className="social-nav-cta">
+        <NavBar />
+        <SocialMedia />
+      </div>
     </div>
   );
 }
