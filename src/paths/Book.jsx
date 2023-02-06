28d4496549ff6cb5ebcfed3a18342e593e23dc9b
unknown <ksemenza13@gmail.com>
2023-02-06 09:52:29 -0500

    videos uploaded

--- src/paths/Book.jsx
+++ src/paths/Book.jsx
@@ -1,15 +1,39 @@
 import '../assets/css/path.css';
 
+
 function Book () {
   return (
-    <div className="path-cta book-cta">
-        <h2 className="path-h2">Book</h2>
-        <form>
-          
-        </form>
-
+    <div className="contact-frame">
+        <div className="contact-header">
+        <h3 className="contact-h3"> Contact </h3>
+        </div>
+        
+        <div className="contact-content content-frame">
+            <form className="contact-form">
+            <p>
+                
+                Name<br/>
+                <input name="name" type="text"/>
+                    <br/>
+                    Company<br/>
+                <input name="company" type="text"/>
+                    <br/>                
+                Email<br/>
+                <input name="email" type="email"/>
+                    <br/>
+                Phone<br/>
+                <input name="phone" type="phone"/>
+                    <br/>
+                    <br/>
+                Questions/Comments<br/>
+                <textarea label="comments" name="comments"/>
+                    <br/>
+            </p>
+                <br/>                                        
+            </form>
+        </div>
     </div>
-  );
+)
 }
 
 export default Book;
